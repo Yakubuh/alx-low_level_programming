98ea112fc5bddb13cbbdc9bb9a139440f93775d0
Bayulu Yakubu <105476695+Yakubuh@users.noreply.github.com>
2022-09-13 15:27:40 +0000

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -21,3 +21,13 @@ int main(void)
 	return (0);
 }
 void jack_bauer(void)
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
