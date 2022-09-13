d4623fc8c37a38a8b86fd4787a62b11ca18f4bb1
Bayulu Yakubu <105476695+Yakubuh@users.noreply.github.com>
2022-09-13 13:17:10 +0000

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,24 +1,23 @@
 #include "stdio.h"
-#include "main.h"
-#include "holberton.h"
+
 /**
  * print_alphabet - prints alphabet
  *
  * Return: always 0;
  *
  */
-void print_alphabet(void)
+
+int main (void)
 {
+	
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		printf("%c",i);
+		printf(" ");
 	}
 	printf("\n");
-}
-int main (void)
-{
-	print_alphabet();
+	
 	return 0;
 }
