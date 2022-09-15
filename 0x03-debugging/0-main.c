836645142ed0182b659a3f78aa7780d3da206cd5
Bayulu Yakubu <105476695+Yakubuh@users.noreply.github.com>
2022-09-15 08:26:15 +0000

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,3 @@
-#include<stdio.h>
 #include "main.h"
 
 /**
@@ -11,7 +10,7 @@ int main(void)
         int i;
 
         i = 98;
-        positive_or_negative(i);
+     //   positive_or_negative(i);
 
         return (0);
 }
