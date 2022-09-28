47d31654fa500dc093a0dc0fbd9e57bd4a2ddb02
Bayulu Yakubu <105476695+Yakubuh@users.noreply.github.com>
2022-09-28 08:04:19 +0000

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,10 +8,10 @@
 int main (void)
 {
 	
-char name = "Puts with recursion";
-	_puts_recursion("%s",  name);
+
+	_puts_recursion("Puts with recursion");
 	
 	_putchar('\n');
 	
-	return 0;
+	return (0);
 }
