9b5db170d786c1e719e452eb13d7a21353cd2e66
Bayulu Yakubu <105476695+Yakubuh@users.noreply.github.com>
2022-09-28 08:42:15 +0000

    Update main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,12 +5,7 @@ void _puts_recursion(char *s);
 
 void _print_rev_recursion(char *s);
 
-int _strlen_recursion(char *s)
-  int _strlen_recursion(char *s){
-
- return strlen(s);
- }
-
+int _strlen_recursion(char *s);
 
 int factorial(int n);
 
