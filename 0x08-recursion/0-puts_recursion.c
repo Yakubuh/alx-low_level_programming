4cfb560703f8d6058c24c5e9308efcd8199fccf3
Bayulu Yakubu <105476695+Yakubuh@users.noreply.github.com>
2022-09-28 19:58:52 +0000

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,25 +1,17 @@
 
-#include <stdio.h>
-
+#include "main.h"
 
 /**
- * _puts_recursion - uses recursion
- * @s: string to recurse through
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: string to print
  */
-
 void _puts_recursion(char *s)
- {
-
-
- printf("%s", s);
-
- }
-
-int main (void)
 {
-
-
-	_puts_recursion("First, solve the problem. Then, write the code \n");
-	
-	return (0);
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
