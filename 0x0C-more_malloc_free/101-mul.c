5e7169172a83d643ec577817c0f1c5c644b535f0
Bayulu Yakubu <105476695+Yakubuh@users.noreply.github.com>
2022-10-06 20:29:07 +0000

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int multplication(){
+int num1;
+int num2;
+int mul;
+
+mul = num1*num2;
+
+printf("%d \n", mul);
+
+if (mul != num1*num2 )
+{
+printf("Error \n");
+
+return (98);
+
+}
+
+
+}
+
+int main()
+{
+
+ multplication(10,20);
+
+    return 0;
+}
 
